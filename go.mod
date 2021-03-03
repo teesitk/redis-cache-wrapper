e2293d1eb75014e1756def621dfe104c7a3b5491
teesitk <teesitk@becicorp.com>
2021-03-03 18:29:51 +0700

    cache wrapper

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/teesitk/redis-cache-wrapper
 
 go 1.13
+
+require github.com/go-redis/redis/v8 v8.6.0 // indirect
--- redis-cache-wrapper.go
+++ redis-cache-wrapper.go
@@ -1,7 +1,34 @@
 package redisCacheWrapper
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var ctx = context.Background()
+var client = &redis.Client{}
 
 func Hello() {
 	fmt.Println("Hello, World!")
+}
+func Get(key string) *redis.StringCmd {
+	return client.Get(ctx, key)
+}
+
+func Set(key string, value interface{}, expiredAt time.Duration) *redis.StatusCmd {
+	return client.Set(ctx, key, value, expiredAt)
+}
+
+func Remember(key string, value interface{}, expiredAt time.Duration, executor func() interface{}) interface{} {
+	result := client.Get(ctx, key)
+	if result != nil {
+		return result
+	} else {
+		result := executor()
+		client.Set(ctx, key, result, expiredAt)
+		return result
+	}
 }
